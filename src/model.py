0b0aac29617718607757be5a27acfd61e4afbdc6
DanielD0304 <daniel.daenecke@gmx.de>
2025-12-22 23:05:39 +0100

    fix: double softmax problem

--- src/model.py
+++ src/model.py
@@ -10,13 +10,11 @@ class Model(nn.Module):
     def __init__(self, d_model, num_heads, num_layers):
         super(Model, self).__init__()
         self.transformer = Transformer(d_model, num_heads, num_layers)
-        self.softmax = nn.Softmax(dim = -1)
         
         
     def forward(self, x, positions, mask=None):
         output, _ = self.transformer(x, positions, mask)
-        output_pred = self.softmax(output)
-        return output_pred
+        return output
     
     def get_attention_weights(self, x, positions, mask=None):
         _, attn_weights = self.transformer(x, positions, mask)
